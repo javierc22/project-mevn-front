3a2ace46e0b4c7c5905939fad36ef5b473861a06
javierc22 <javier.ec.22@gmail.com>
2020-04-05 15:46:24 -0400

    Axios y ruta GET

--- src/views/Notas.vue
+++ src/views/Notas.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Notas</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Descripción</th>
+          <th scope="col">Fecha</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in notas" :key="index">
+          <th scope="row">{{ item._id }}</th>
+          <td>{{ item.nombre }}</td>
+          <td>{{ item.descripcion }}</td>
+          <td>{{ item.date }}</td>
+          <td>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      notas: [],  
+    }
+  },
+  created() {
+    this.listarNotas();
+  },
+  methods: {
+    listarNotas(){
+      this.axios.get('/notas')
+        .then(res => {
+          console.log(res.data);
+          this.notas = res.data;
+        })
+        .catch(e => {
+          console.log(e.response);
+        })
+    }
+  },
+}
+</script>
