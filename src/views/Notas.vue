fdcc3e974e8f650b4c585a834b390995928fa3b8
javierc22 <javier.ec.22@gmail.com>
2020-04-05 17:04:32 -0400

    Axios y ruta PUT

--- src/views/Notas.vue
+++ src/views/Notas.vue
@@ -13,11 +13,20 @@
     </b-alert>
 
     <!-- Formulario Agregar Nota -->
-    <form @submit.prevent="agregarNota()">
+    <form @submit.prevent="agregarNota()" v-if="!editar">
       <h3>Agregar nueva nota</h3>
       <input type="text" class="form-control my-2" placeholder="Nombre" v-model="nota.nombre">
       <input type="text" class="form-control my-2" placeholder="Descripción" v-model="nota.descripcion">
       <b-button class="btn-success my-2 btn-block" type="submit">Agregar</b-button>
+    </form>
+
+    <!-- Formulario Editar Nota -->
+    <form @submit.prevent="editarNota(notaEditar)" v-if="editar">
+      <h3>Editar nota</h3>
+      <input type="text" class="form-control my-2" placeholder="Nombre" v-model="notaEditar.nombre">
+      <input type="text" class="form-control my-2" placeholder="Descripción" v-model="notaEditar.descripcion">
+      <b-button class="btn-warning my-2 mx-2" type="submit">Editar</b-button>
+      <b-button class="my-2" @click="editar = false">Cancelar</b-button>
     </form>
     
     <!-- Tabla -->
@@ -39,10 +48,10 @@
           <td>{{ item.date }}</td>
           <td>
             <!-- <b-button @click="alerta()">Acción</b-button> -->
-            <b-button 
-              class="btn-danger" 
-              @click="eliminarNota(item._id)"
-            >
+            <b-button class="btn-warning btn-sm mx-2" @click="activarEdicion(item._id)">
+              Editar
+            </b-button>
+            <b-button class="btn-danger btn-sm" @click="eliminarNota(item._id)">
               Eliminar
             </b-button>
           </td>
@@ -60,7 +69,9 @@ export default {
       mensaje: {color: '', texto: ''},
       dismissSecs: 5,
       dismissCountDown: 0,
-      nota: { nombre: '', descripcion: ''}
+      nota: { nombre: '', descripcion: ''},
+      editar: false,
+      notaEditar: {},
     }
   },
   created() {
@@ -112,6 +123,31 @@ export default {
           this.mensaje.color = 'success';
           this.mensaje.texto = 'Nota eliminada!';
           this.showAlert();
+        })
+        .catch(e => {
+          console.log(e.response);
+        })
+    },
+    activarEdicion(id){
+      this.editar = true;
+      this.axios.get(`/nota/${id}`)
+        .then(res => {
+          this.notaEditar = res.data;
+        })
+        .catch( e => {
+          console.log(e.response);
+        })
+    },
+    editarNota(item){
+      this.axios.put(`/nota/${item._id}`, item)
+        .then(res => {
+          const index = this.notas.findIndex( n => n._id === res.data._id);
+          this.notas[index].nombre = res.data.nombre;
+          this.notas[index].descripcion = res.data.descripcion;
+          this.mensaje.color = 'success';
+          this.mensaje.texto = 'Nota editada!';
+          this.showAlert();
+          this.editar = false;
         })
         .catch(e => {
           console.log(e.response);
