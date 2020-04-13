075ed37db6a57c666eb48d2320c6d2a05cca938a
javierc22 <javier.ec.22@gmail.com>
2020-04-12 22:02:33 -0400

    Navbar con VueBootstrap

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,25 @@
       <a @click="cerrarSesion()" v-if="estaActivo">Cerrar sesión</a> |
       <router-link to="/notas" v-if="estaActivo">Notas</router-link>
     </div>
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+          <b-nav-item to="/login" v-if="!estaActivo">Login</b-nav-item>
+          <b-nav-item to="/notas" v-if="estaActivo">Notas</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item @click="cerrarSesion()" v-if="estaActivo">Cerrar sesión</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
