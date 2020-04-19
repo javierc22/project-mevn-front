1e58e20da5e98ce8f2cf510f83b21a70aeadfe4f
javierc22 <javier.ec.22@gmail.com>
2020-04-19 16:44:48 -0400

    Conceptos claves de Querys con Router-link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <router-link :to="{query: {seccion: 1}}" class="btn btn-primary btn-block">
+      1
+    </router-link>
+
+    <router-link :to="{query: {seccion: 2}}" class="btn btn-primary btn-block">
+      2
+    </router-link>
   </div>
 </template>
+
+<script>
+export default {
+  watch: {
+    "$route.query.seccion": {
+      inmediate: true,
+      handler(newVal, oldVal){
+        console.log(`Sección a cambiado a: ${newVal}`)
+      }
+    }
+  },
+}
+</script>
